9f4fc8b26572f6614e0dd2c15fc7de03623e6a52
Liron Rotman <lironr967@gmail.com>
2024-11-01 18:50:30 +0200

    .

--- VoodoProject/Assets/Scripts/LevelUp/Data/TowerCostSO.cs
+++ VoodoProject/Assets/Scripts/LevelUp/Data/TowerCostSO.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+[CreateAssetMenu(fileName = "TowerCostSO", menuName = "ScriptableObjects/Tower/Cost")]
 public class TowerCostSO : ScriptableObject
 {
     [System.Serializable]
--- VoodoProject/Assets/Scripts/LevelUp/Data/TowerSO.cs
+++ VoodoProject/Assets/Scripts/LevelUp/Data/TowerSO.cs
@@ -2,11 +2,16 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+[CreateAssetMenu(fileName = "TowerSO", menuName = "ScriptableObjects/Tower/Level")]
 public class TowerSO : ScriptableObject
 {
+    [SerializeField] Sprite _towerSprite;
+    [SerializeField] Sprite _gunSprite;
     [SerializeField] float _attackRate;
     [SerializeField] float _damage;
 
     public float AttackRate=> _attackRate;
     public float Damage => _damage;
+    public Sprite TowerSprit => _towerSprite;
+    public Sprite GunSprite => _towerSprite;
 }
