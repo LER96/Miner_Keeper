f3fe210af45472ae39793fdbfce741a28691f6c3
Liron Rotman <lironr967@gmail.com>
2024-11-03 14:01:44 +0200

    .

--- VoodoProject/Assets/Scripts/LevelUp/Data/TowerSO.cs
+++ VoodoProject/Assets/Scripts/LevelUp/Data/TowerSO.cs
@@ -10,10 +10,12 @@ public class TowerSO : ScriptableObject
     [SerializeField] Sprite _gunSprite;
     [SerializeField] float _attackRate;
     [SerializeField] float _damage;
+    [SerializeField] float _rotationSpeed;
 
     public int TowerLevel => _level;
     public float AttackRate=> _attackRate;
     public float Damage => _damage;
+    public float Agility => _rotationSpeed;
     public Sprite TowerSprit => _towerSprite;
     public Sprite GunSprite => _towerSprite;
 }
